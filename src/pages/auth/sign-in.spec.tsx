8a24e6c88f8bbfb795fb4a9fcd0f1a578b126970
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-04 07:50:40 -0300

    feat: create unit test to SignIn form

---
+++ src/pages/auth/sign-in.spec.tsx
@@ -0,0 +1,37 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { HelmetProvider } from 'react-helmet-async'
+import { MemoryRouter } from 'react-router-dom'
+
+import { queryClient } from '@/lib/react-query'
+
+import { SignIn } from './sign-in'
+
+describe('SignIn', () => {
+  it('should set default email input value if is email is present on search param', () => {
+    const wrapper = render(
+      <>
+        <SignIn />
+      </>,
+      {
+        wrapper: ({ children }) => {
+          return (
+            <HelmetProvider>
+              <MemoryRouter
+                initialEntries={['/sign-in?email=hudsonholanda55@gmail.com']}
+              >
+                <QueryClientProvider client={queryClient}>
+                  {children}
+                </QueryClientProvider>
+              </MemoryRouter>
+            </HelmetProvider>
+          )
+        },
+      },
+    )
+
+    const emailInput = wrapper.getByLabelText('Seu e-mail') as HTMLInputElement
+
+    expect(emailInput.value).toEqual('hudsonholanda55@gmail.com ')
+  })
+})
