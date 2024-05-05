cb445b921416fb7adb5d71b2d0a83c4fb8d6e01f
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-05 09:20:45 -0300

    feat: type request body

--- src/api/mocks/sign-in-mock.ts
+++ src/api/mocks/sign-in-mock.ts
@@ -1,16 +1,21 @@
 import { http, HttpResponse } from 'msw'
 
-export const signInMock = http.post('/authenticate', async ({ request }) => {
-  const { email } = await request.json()
+import { SignInBody } from '../sign-in'
 
-  if (email === 'hudsonholanda55@gmail.com') {
-    return new HttpResponse(null, {
-      status: 200,
-      headers: {
-        'Set-Cookie': 'auth=sample-jwt',
-      },
-    })
-  }
+export const signInMock = http.post<never, SignInBody>(
+  '/authenticate',
+  async ({ request }) => {
+    const { email } = await request.json()
 
-  return new HttpResponse(null, { status: 401 })
-})
+    if (email === 'hudsonholanda55@gmail.com') {
+      return new HttpResponse(null, {
+        status: 200,
+        headers: {
+          'Set-Cookie': 'auth=sample-jwt',
+        },
+      })
+    }
+
+    return new HttpResponse(null, { status: 401 })
+  },
+)
