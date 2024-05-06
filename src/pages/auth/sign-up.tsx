d060a9faa748f6f023f84a12a0901f48588c03e8
Hudson Holanda <hudsonholanda55@gmail.com>
2024-05-06 09:31:54 -0300

    refact: remove promise time out

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -42,8 +42,6 @@ export function SignUp() {
         phone: data.phone,
       })
 
-      await new Promise((resolve) => setTimeout(resolve, 2000))
-
       toast.success('Restaurante cadastrado com sucesso!', {
         action: {
           label: 'Login',
