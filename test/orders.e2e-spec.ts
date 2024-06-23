794be81c7f2c6b641dbc1aaf8515e2a1100ed1fe
Hudson Holanda <hudsonholanda55@gmail.com>
2024-06-23 11:03:26 -0300

    feat: add await

--- test/orders.e2e-spec.ts
+++ test/orders.e2e-spec.ts
@@ -51,7 +51,7 @@ test('paginate orders', async ({ page }) => {
     page.getByRole('cell', { name: 'Customer 1', exact: true }),
   ).toBeVisible()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 10', exact: true }),
   ).toBeVisible()
 })
@@ -62,7 +62,7 @@ test('filter by order id', async ({ page }) => {
   await page.getByPlaceholder('ID do pedido').fill('order-11')
   await page.getByRole('button', { name: 'Filtrar resultados' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'order-11', exact: true }),
   ).toBeVisible()
 })
@@ -73,7 +73,7 @@ test('filter by customer name', async ({ page }) => {
   await page.getByPlaceholder('Nome do cliente').fill('Customer 11')
   await page.getByRole('button', { name: 'Filtrar resultados' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 11', exact: true }),
   ).toBeVisible()
 })
@@ -83,15 +83,11 @@ test('filter by status', async ({ page }) => {
 
   await page.getByRole('combobox').click()
 
-  await page.waitForTimeout(1000)
-
   await page.getByLabel('Pendente').click()
 
   await page.getByRole('button', { name: 'Filtrar resultados' }).click()
 
-  const tableRows = await page.getByRole('cell', { name: 'Pendente' }).all()
+  const tableRows = await page.getByRole('cell', { name: 'Pendente' })
 
-  expect(tableRows).toHaveLength(10)
-
-  await page.waitForTimeout(1000)
+  await expect(tableRows).toHaveCount(10)
 })
